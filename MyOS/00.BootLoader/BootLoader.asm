d86b4e66664201600bc547352d7d4bad6640d51f
myroomun <myroomun@naver.com>
2015-12-25 03:26:58 +0900

    end of ch.4

--- MyOS/00.BootLoader/BootLoader.asm
+++ MyOS/00.BootLoader/BootLoader.asm
@@ -3,7 +3,41 @@
 
 SECTION .text
 
-jmp $
+jmp 0x7c0:START
+
+START:
+	mov ax, 0x7C0
+	mov ds, ax
+	mov ax, 0xB800
+	mov es, ax
+
+	mov si, 0
+
+.SCREENCLEARLOOP:
+	mov byte[es:si], 0
+	mov byte[es:si+1], 0x0A
+	add si, 2
+	cmp si, 80 * 25 * 2
+	jl .SCREENCLEARLOOP
+
+	mov si, 0
+	mov di, 0
+
+.MESSAGELOOP:
+	mov cl, byte[si+MESSAGE1]
+	cmp cl, 0
+	je .MESSAGEEND
+
+	mov byte[es:di], cl
+	add si, 1
+	add di, 2
+
+	jmp .MESSAGELOOP
+
+.MESSAGEEND:
+	jmp $
+
+MESSAGE1:	db 'MyOS is started!',0
 
 times 510 - ( $ - $$ )		db		0x00
 
