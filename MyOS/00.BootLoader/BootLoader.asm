29044b90625bc020f2b34513ea618a6b7afdee1f
myroomun <myroomun@naver.com>
2015-12-25 04:28:34 +0900

    End of Ch.5

--- MyOS/00.BootLoader/BootLoader.asm
+++ MyOS/00.BootLoader/BootLoader.asm
@@ -5,11 +5,18 @@ SECTION .text
 
 jmp 0x7c0:START
 
+TOTALSECTORCOUNT:	dw	1024
+
 START:
 	mov ax, 0x7C0
 	mov ds, ax
 	mov ax, 0xB800
 	mov es, ax
+
+	mov ax, 0x00
+	mov ss, ax
+	mov sp, 0xFFFE
+	mov bp, 0xFFFE
 
 	mov si, 0
 
@@ -20,11 +27,107 @@ START:
 	cmp si, 80 * 25 * 2
 	jl .SCREENCLEARLOOP
 
-	mov si, 0
-	mov di, 0
+	push MESSAGE1
+	push 0
+	push 0
+	call PRINTMESSAGE
+	add sp, 6
+
+	push IMAGELOADINGMESSAGE
+	push 1
+	push 0
+	call PRINTMESSAGE
+	add sp, 6
+
+RESETDISK:
+	mov ax, 0
+	mov dl, 0
+	int 0x13
+	jc HANDLEDISKERROR
+
+	mov si, 0x1000 ; addr setting to save OS img
+	mov es, si
+	mov bx, 0x00
+
+	mov di, word[TOTALSECTORCOUNT]
+
+READDATA:
+	cmp di, 0
+	je READEND
+	sub di, 0x01
+
+	mov ah, 0x02
+	mov al, 0x1
+	mov ch, byte[TRACKNUMBER]
+	mov cl, byte[SECTORNUMBER]
+	mov dh, byte[HEADNUMBER]
+	mov dl, 0x00
+	int 0x13
+	jc HANDLEDISKERROR
+
+	add si, 0x20 ; add save addr with 512bytes
+	mov es, si
+
+	mov al, byte[SECTORNUMBER]
+	add al, 0x01
+	mov byte[SECTORNUMBER], al
+	cmp al, 19
+	jl READDATA
+
+	xor byte[HEADNUMBER], 0x01
+	mov byte[SECTORNUMBER], 0x01
+
+	cmp byte[HEADNUMBER], 0x00
+	jne READDATA
+
+	add byte[TRACKNUMBER], 0x01
+	jmp READDATA
+
+READEND:
+	push LOADINGCOMPLETEMESSAGE
+	push 1
+	push 20
+	call PRINTMESSAGE
+	add sp, 6
+
+	jmp 0x1000:0x0000
+
+HANDLEDISKERROR:
+	push DISKERRORMESSAGE
+	push 1
+	push 20
+	call PRINTMESSAGE
+
+	jmp $
+
+PRINTMESSAGE:
+	push bp
+	mov bp, sp
+
+	push es
+	push si
+	push di
+	push ax
+	push cs
+	push dx
+
+	mov ax, 0xB800
+	mov es, ax
+
+	mov ax, word[bp+6]
+	mov si, 160
+	mul si
+	mov di, ax
+
+	mov ax, word[bp+4]
+	mov si, 2
+	mul si
+	add di, ax
+
+	mov si, word[bp+8]
 
 .MESSAGELOOP:
-	mov cl, byte[si+MESSAGE1]
+	mov cl, byte[si]
 	cmp cl, 0
 	je .MESSAGEEND
 
@@ -35,9 +138,24 @@ START:
 	jmp .MESSAGELOOP
 
 .MESSAGEEND:
-	jmp $
+	pop dx
+	pop cx
+	pop ax
+	pop di
+	pop si
+	pop es
+	pop bp
+	ret
 
 MESSAGE1:	db 'MyOS is started!',0
+
+DISKERRORMESSAGE:		db	'Disk Error', 0
+IMAGELOADINGMESSAGE:	db	'OS Image Loading', 0
+LOADINGCOMPLETEMESSAGE:	db	'Complete', 0
+
+SECTORNUMBER:			db	0x02
+HEADNUMBER:				db	0x00
+TRACKNUMBER:			db	0x00
 
 times 510 - ( $ - $$ )		db		0x00
 
